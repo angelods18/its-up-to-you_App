32ec8539430e602563c0cdb1ca534f2e8c27c894
angelo <angelods18@gmail.com>
2023-03-29 23:29:00 +0200

    add android config to debug on emulator

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.itsup2u.app',
   appName: 'itsup2u',
   webDir: 'www',
   bundledWebRuntime: false
